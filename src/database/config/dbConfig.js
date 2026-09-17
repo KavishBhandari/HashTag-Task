626064a1e7fae11e6a85a5259458ad9cda5d9bcf
kavi <kavishbhandari5@gmail.com>
2026-09-17 17:19:11 +0530

    docker file added

--- src/database/config/dbConfig.js
+++ src/database/config/dbConfig.js
@@ -4,9 +4,11 @@ const ApiError = require("../../utils/apiError");
 
 const connectDatabase = async () => {
     try {
+        console.log("connectDatabase funtioon is called::::::::")
         await mongoose.connect(process.env.DB_CONNECTION_URL || "mongodb://localhost:27017/mongodbLearning");
+        console.log("database conneted successfully....")
     } catch (error) {
-        console.error(error)
+        console.error("🔴 Database connection error:", error);
         throw new ApiError(500, "Database connection error.")
     }
 };
